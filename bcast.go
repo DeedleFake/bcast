1331cfa7740cc40773a37f2154e51b9609853c91
DeedleFake <deedlefake@users.noreply.github.com>
2019-09-21 22:27:02 -0400

    Godoc comments and fix a bug.

--- bcast.go
+++ bcast.go
@@ -6,7 +6,9 @@ import (
 
 // Broadcast is a N:M channel-based broadcaster. It allows for
 // event-like broadcasts of arbitrary data to multiple receivers
-// simultaneously, with guarantees on ordering and receipt.
+// simultaneously. The order that data is sent in is guarunteed to be
+// received in that order by any listeners, but the order that the
+// listeners receive the data in relative to each other is not.
 //
 // The zero-value is valid and ready for use, but clients should make
 // sure that they don't use multiple copies of the same instance.
@@ -63,10 +65,26 @@ func (bc *Broadcast) coord() {
 	}
 }
 
+// Listen registers c as a listening channel, meaning that any
+// broadcasts sent after this call will be replicated to c. The
+// returned function unregisters c.
+//
+// When a listening channel is done, either becuase the returned stop
+// function was called or because the entire broadcaster was stopped,
+// it is closed.
+//
+// This function will block if an existing broadcast is in progress.
+//
+// This function is a no-op if the broadcaster has been stopped, as is
+// calling the returned stop function.
 func (bc *Broadcast) Listen(c chan<- interface{}) (stop func()) {
 	bc.init()
 
-	bc.listen <- c
+	select {
+	case <-bc.done:
+	case bc.listen <- c:
+	}
+
 	return func() {
 		select {
 		case <-bc.done:
@@ -75,12 +93,22 @@ func (bc *Broadcast) Listen(c chan<- interface{}) (stop func()) {
 	}
 }
 
+// Send returns a channel which broadcasts anything sent to it to all
+// listening channels. Sending to this channel will block if any
+// existing broadcasts are in progress. A broadcast is considered to
+// be in progress until all listening channels have been sent to
+// succesfully.
+//
+// It is the callers responsibility to not send anything to the
+// channel returned by Send after the broadcaster has been stopped.
 func (bc *Broadcast) Send() chan<- interface{} {
 	bc.init()
 
 	return bc.broadcast
 }
 
+// Stop stops the broadcaster, resulting in all listening channels
+// being closed and the background coordinator being stopped.
 func (bc *Broadcast) Stop() {
 	bc.init()
 
